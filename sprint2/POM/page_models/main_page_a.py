715cc85d55c0cfc73759ab6a7c54ce892267a4ba
agnes77horvath_glitch <agnes77.horvath@gmail.com>
2026-06-27 16:03:07 +0200

    sprint2/test_ures_kereses.py, sprint1 és sprint2/test_404_page.py, sprint2/test_language_english.py módosítása

--- sprint2/POM/page_models/main_page_a.py
+++ sprint2/POM/page_models/main_page_a.py
@@ -12,12 +12,28 @@ class MoovSmartMain(GeneralPage):
 
     SEARCH_INPUT = (By.CSS_SELECTOR, "input.geoapify-autocomplete-input")
     SUGGESTION_ITEM = (By.CSS_SELECTOR, ".geoapify-autocomplete-item")
+    SIGN_IN = (By.XPATH, "//a[normalize-space()='Sign In']")
+    REGISTRATION = (By.XPATH, "//a[normalize-space()='Registration']")
+    SEARCH_BUTTON = (By.XPATH, "//button[normalize-space()='Search']")
+    PROPERTY_TYPE = (By.ID, "propertyType")
+
+    #def rent_button(self):
+    #    return self.browser.find_element(By.XPATH, " (//a[@class='navlink hover-underline-animation'])[2]")
 
     def rent_button(self):
-        return self.browser.find_element(By.XPATH, " (//a[@class='navlink hover-underline-animation'])[2]")
+        return WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located((By.LINK_TEXT, "Rent")))
 
-    def sing_in_button(self):
-        return self.browser.find_element(By.XPATH, "(//div[@class='my-navbar-right-links']/a)[1]")
+    #def sign_in_button2(self):
+    #    return self.browser.find_element(By.XPATH, "(//div[@class='navbar-side-items-link hover-animation']/a)[1]")
+
+    #def sign_in_button(self):
+    #    return WebDriverWait(self.browser, 10).until(
+    #        EC.visibility_of_element_located(self.SIGN_IN))
+
+    def sign_in_button(self):
+        return WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located(self.SIGN_IN))
 
     def email(self):
         return self.browser.find_element(By.XPATH, "//label[@for='email']")
@@ -28,8 +44,12 @@ class MoovSmartMain(GeneralPage):
     def text_center(self):
         return self.browser.find_element(By.XPATH, ('//div[@class ="text-center"]'))
 
+    #def registration_button(self):
+    #    return self.browser.find_element(By.XPATH, "(//div[@class='my-navbar-right-links']/a)[2]")
+
     def registration_button(self):
-        return self.browser.find_element(By.XPATH, "(//div[@class='my-navbar-right-links']/a)[2]")
+        return WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located(self. REGISTRATION))
 
     def registration_word(self):
         return self.browser.find_element(By.XPATH, ('//h3[@class="main-title"]'))
@@ -58,6 +78,8 @@ class MoovSmartMain(GeneralPage):
     def register_button(self):
         return self.browser.find_element(By.XPATH, "//button[@type='submit' and contains(., 'Register')]")
 
+
+
     def search_for_address(self, address):
         search = self.wait.until(EC.presence_of_element_located(self.SEARCH_INPUT))
         search.click()
@@ -69,14 +91,24 @@ class MoovSmartMain(GeneralPage):
         search_button = self.browser.find_element(By.XPATH, '//button[@class="btn header-search-button"]')
         search_button.click()
 
+    #def select_language(self, language):
+    #    Select(self.browser.find_element(By.XPATH, "//app-nav/select")).select_by_visible_text(language)
+
     def select_language(self, language):
-        Select(self.browser.find_element(By.XPATH, "//app-nav/select")).select_by_visible_text(language)
+        dropdown = WebDriverWait(self.browser, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//app-nav/select"))
+        )
+        Select(dropdown).select_by_visible_text(language)
 
     def get_search_placeholder(self):
         return self.browser.find_element(*self.SEARCH_INPUT).get_attribute("placeholder")
 
+    #def get_buy(self):
+    #    return self.browser.find_element(By.XPATH, "(//a[@class='navlink hover-underline-animation'])[1]")
+
     def get_buy(self):
-        return self.browser.find_element(By.XPATH, "(//a[@class='navlink hover-underline-animation'])[1]")
+        return WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located((By.LINK_TEXT, "Buy")))
 
     def header_title(self):
         return self.browser.find_element(By.XPATH, '//div[@class="header-title"]')
@@ -93,8 +125,13 @@ class MoovSmartMain(GeneralPage):
     def city_placeholder(self):
         return self.browser.find_element(By.ID, "city")
 
+    #def search_button(self):
+    #    return self.browser.find_element(By.XPATH, '//button[@class="btn main-form-btn"]')
+
     def search_button(self):
-        return self.browser.find_element(By.XPATH, '//button[@class="btn main-form-btn"]')
+        return WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located(self.SEARCH_BUTTON)
+        )
 
     def helymeghatarozok_szama(self):
         return len(self.browser.find_elements(By.CSS_SELECTOR,"img.leaflet-marker-icon"))
@@ -109,3 +146,7 @@ class MoovSmartMain(GeneralPage):
             #ha megkapja a "nincs találat" üzenetet akkor igazt (True) ad vissza
         except:
             return False
+
+    def property_type(self):
+        return WebDriverWait(self.browser, 10).until(
+            EC.visibility_of_element_located(self.PROPERTY_TYPE))

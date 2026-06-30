b7785485ea418b422540685577a13bb8677ce5c3
SkywalkerLajos-tester <szabozoli0903@gmail.com>
2026-06-30 14:25:24 +0200

    Sprint1 - CI/CD v1

--- sprint2/POM/create_driver.py
+++ sprint2/POM/create_driver.py
@@ -1,22 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-def get_configured_chrome_driver():
-    options = Options()
-    options.add_experimental_option('detach', True)
-    options.add_argument('--guest')
-    options.add_argument("--lang=hu")
-    options.add_argument("--headless")
-    browser = webdriver.Chrome(options=options)
-    browser.set_window_size(1920, 1080)
-    return browser
-
-
 # def get_configured_chrome_driver():
 #     options = Options()
 #     options.add_experimental_option('detach', True)
 #     options.add_argument('--guest')
 #     options.add_argument("--lang=hu")
+#     options.add_argument("--headless")
 #     browser = webdriver.Chrome(options=options)
-#     browser.maximize_window()
+#     browser.set_window_size(1920, 1080)
 #     return browser
+
+
+def get_configured_chrome_driver():
+    options = Options()
+    options.add_experimental_option('detach', True)
+    options.add_argument('--guest')
+    options.add_argument("--lang=hu")
+    browser = webdriver.Chrome(options=options)
+    browser.maximize_window()
+    return browser
